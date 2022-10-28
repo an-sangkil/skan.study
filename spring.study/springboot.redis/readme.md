7e6f734072416bfdcd0441dcabbbe40f1f37f62c
sangkil.an <sangkil.an@cj.net>
2022-10-28 16:55:23 +0900

    폴더 이름 변경

---
+++ spring.study/springboot.redis/readme.md
@@ -0,0 +1,4 @@
+# Redis Pub-sub Example
+- 레디스를 이용한 간단한 Queue 프로그래밍
+
+# 테스트 URL
---
+++ spring.study/springboot.redis/src/main/java/com/skan/controller/Test.java
@@ -0,0 +1,12 @@
+package com.skan.controller;
+
+public class Test { 
+
+
+    public static void main(String[] args) {
+        String str= "ssss";
+        System.out.print(str)
+                                                            
+        
+    }
+}
