21f8464b7e629bf0beabd0f4209987ff688f743b
sangkil.an <sangkil.an@cj.net>
2022-12-20 11:26:35 +0900

    factory method

--- java.study/src/main/java/designpattern/creation/factory/methodfactory/ex3_1/factory/PizzaFactory.java
+++ java.study/src/main/java/designpattern/creation/factory/methodfactory/ex3_1/factory/PizzaFactory.java
@@ -2,15 +2,8 @@ package designpattern.creation.factory.methodfactory.ex3_1.factory;
 
 import designpattern.creation.factory.methodfactory.ex3_1.product.Pizza;
 
-/**
- * <pre>
- * Description :
- *
- *
- * </pre>
- *
+ /**
  * @author skan
- * @version Copyright (C) 2022 by CJENM|MezzoMedia. All right reserved.
  * @since 2022/12/09
  */
 public abstract class PizzaFactory {
