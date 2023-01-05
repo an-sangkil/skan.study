e54a45dd6f5ff2f9a72f0c5cf1f46f7bd2d31408
sangkil.an <sangkil.an@cj.net>
2023-01-05 10:48:44 +0900

    chain of responsibility pattern

---
+++ java.study/src/main/java/designpattern/beahvoir/chain/Dollar10Dispenser.java
@@ -0,0 +1,36 @@
+package designpattern.beahvoir.chain;
+
+/**
+ * <pre>
+ * Description :
+ *
+ *
+ * </pre>
+ *
+ * @author skan
+ * @since 2023/01/03
+ */
+public class Dollar10Dispenser implements DispenseChain{
+    private DispenseChain chain;
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        this.chain = nextChain;
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+
+        if (currency.getAmount() >= 10) {
+            int num = currency.getAmount()/10;
+            int remainder = currency.getAmount() % 10;
+            System.out.println("분배 : " + num + " * 10$ ");
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(currency);
+        }
+
+    }
+}
---
+++ java.study/src/main/java/designpattern/beahvoir/chain/Dollar50Dispenser.java
@@ -0,0 +1,37 @@
+package designpattern.beahvoir.chain;
+
+/**
+ * <pre>
+ * Description :
+ *
+ *
+ * </pre>
+ *
+ * @author skan
+ * @version Copyright (C) 2023 by CJENM|MezzoMedia. All right reserved.
+ * @since 2023/01/03
+ */
+public class Dollar50Dispenser implements DispenseChain{
+
+    private  DispenseChain chain;
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        this.chain = nextChain;
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+
+        if (currency.getAmount() >= 50) {
+            int num = currency.getAmount()/50;
+            int remainder = currency.getAmount() % 50;
+            System.out.println("분배 : " + num + " * 50$ ");
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(currency);
+        }
+
+    }
+}
