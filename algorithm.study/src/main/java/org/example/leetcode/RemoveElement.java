d1147317d693ace82ac7770138bf55e9c21aa804
skan <an.sangkil@gmail.com>
2025-12-12 19:54:30 +0900

    add: leetcode level-1

--- algorithm.study/src/main/java/org/example/leetcode/RemoveElement.java
+++ algorithm.study/src/main/java/org/example/leetcode/RemoveElement.java
@@ -2,7 +2,13 @@ package org.example.leetcode;
 
 public class RemoveElement {
 
-
+    /**
+     * 투포인터를 활용한 엘리멘트 값 삭제
+     *
+     * @param nums  제공되는 배열
+     * @param val 로 들어오는 인자 값에 대한 삭제
+     * @return
+     */
     public int  removeElement(int[] nums, int val) {
 
         int k=0;
